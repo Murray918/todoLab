958712411fd9dd7ef0d29b2136246def6d7ac8d3
Andrew Murray <darbyguitars@gmail.com>
2019-01-03 16:22:06 -0600

    adding the timing functiions

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 console.log('it is here')
 
+// in this branch we are goin to deal with set interval and set timeout both are similar funcitons
+
+waitForIt = function() {
+	// code goes here
+}
+
+createTimeLoop = function() {
+	//initialize a count for display purposes
+	let count = 0
+	// setinterval runs it call back every 1000 ms
+	setInterval(function() {
+		//do something
+		console.log(`SETINTERVAL HAS RUN ${count} TIMES`)
+		//the second
+	}, 1000)
+}
+
 const moveItem = function(event) {
 	console.log(event.target.parentElement)
 	$('#done-list').append(event.target.parentElement)
@@ -7,13 +24,10 @@ const moveItem = function(event) {
 }
 
 $('#add-task-button').click(function() {
-	//this declares task
 	let task = $('#add-task-input').val()
 	let $button = $('<button>Done</button>').click(moveItem)
-	//put conditional here
 	if (task === '') {
 		alert('Task cannot be blank')
-		// every thing else
 	} else {
 		let $li = $('<li>' + task + '</li>')
 		$('#to-do-list').append($li)
